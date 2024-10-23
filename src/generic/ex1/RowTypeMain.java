0eba83b7dc76b9896b893803b4a8beb8cdad2e96
mindotori <148763440+mindotori@users.noreply.github.com>
2024-10-24 01:12:18 +0900

    제네릭  Row타입

---
+++ src/generic/ex1/RowTypeMain.java
@@ -0,0 +1,12 @@
+package generic.ex1;
+
+public class RowTypeMain {
+
+    public static void main(String[] args) {
+        GenericBox integerBox = new GenericBox(); //제네릭 타입 안넣어도 되긴 함
+        //GenericBox<Object> integerBox = new GenericBox<>(); //권장
+        integerBox.set(10); //근데 Object로 인식
+        Integer result = (Integer) integerBox.get();
+        System.out.println("result = " + result);
+    }
+}
